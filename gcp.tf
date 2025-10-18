885e0c7cf6776b648318fc36363f3c202674761d
JessicaApBueno <buenojessicaaparecida@gmail.com>
2025-10-18 19:47:07 -0300

    fix: Usa usuario padrao 'ubuntu' no GCP e Ansible para resolver erro de permissao SSH

--- gcp.tf
+++ gcp.tf
@@ -1,4 +1,4 @@
-# Este arquivo é dedicado à infraestrutura do GCP
+
 
 # Configura o provider GCP para usar sua chave de serviço
 provider "google" {
@@ -8,8 +8,9 @@ provider "google" {
 }
 
 # --- Data Source: Encontra o ID da Imagem Ubuntu mais Recente ---
-# Garante que o Terraform sempre use o nome de imagem Ubuntu 20.04 correto
+# Garante que o Terraform sempre use a imagem correta.
 data "google_compute_image" "ubuntu_lts" {
+  # Nome da família de imagens mais estável (Ubuntu 22.04 LTS)
   family  = "ubuntu-2204-lts"
   project = "ubuntu-os-cloud" 
 }
@@ -17,12 +18,13 @@ data "google_compute_image" "ubuntu_lts" {
 # --- RECURSO 1: Regra de Firewall para HTTP (Porta 80) ---
 resource "google_compute_firewall" "http_firewall" {
   name    = "allow-http-nginx-petshop"
-  network = "default" 
+  network = "default" # Usa a rede VPC padrão
 
   allow {
     protocol = "tcp"
     ports    = ["80"]
   }
+
   source_ranges = ["0.0.0.0/0"] 
   target_tags = ["http-server"]
 }
@@ -30,18 +32,19 @@ resource "google_compute_firewall" "http_firewall" {
 # --- RECURSO 2: Máquina Virtual (VM) e2-micro (Always Free Tier) ---
 resource "google_compute_instance" "web_server" {
   name         = "petshop-nginx-vm"
-  machine_type = "e2-micro" 
+  machine_type = "e2-micro"
   zone         = "us-central1-a"
 
   # Configuração de Boot Disk
   boot_disk {
     initialize_params {
-      # REFERÊNCIA CORRETA: Usa o self_link da fonte de dados encontrada (já estava certo)
+      # REFERÊNCIA CORRETA: Usa o self_link da fonte de dados encontrada
       image = data.google_compute_image.ubuntu_lts.self_link
       size = 10 
     }
   }
 
+  # Configuração da Placa de Rede
   network_interface {
     network = "default"
     access_config {} 
@@ -49,9 +52,9 @@ resource "google_compute_instance" "web_server" {
 
   tags = ["http-server"]
 
-  # Adiciona a chave SSH de DEPLOY (github_deploy_key.pub)
   metadata = {
-    ssh-keys = "ansible_user:{{ file(\"~/.ssh/github_deploy_key.pub\") }}" 
+    # Isso resolve o erro de "Permission denied" ao usar o Ansible
+    ssh-keys = "ubuntu:{{ file(\"~/.ssh/github_deploy_key.pub\") }}" 
   }
 
   lifecycle {
@@ -59,7 +62,8 @@ resource "google_compute_instance" "web_server" {
   }
 }
 
-# --- RECURSO 3: Saída do IP Público (Usado nas Secrets do GitHub) ---
+# --- RECURSO 3: Saída do IP Público ---
 output "external_ip" {
   value = google_compute_instance.web_server.network_interface.0.access_config.0.nat_ip
 }
+
